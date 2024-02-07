05158afac653706226481d93d25af6552ebfa285
Tristan Mitchell <Tristan.Mitchell@ibm.com>
2024-02-07 14:43:00 +0000

    message

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,5 @@
-import './globals.css';
+import './globals.scss';
+import { Providers } from './providers';
 
 export const metadata = {
   title: 'Carbon + Next13',
@@ -8,7 +9,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <Providers>{children}</Providers>
     </html>
   );
 }
